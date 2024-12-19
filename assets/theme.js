2bf3fce47f0d0a6fe747e86ff0778fe16664a3b8
Pierre Glaser <pierreglaser@msn.com>
2024-12-19 17:49:12 +0000

    minor tweaks

--- assets/theme.js
+++ assets/theme.js
@@ -1,18 +1,35 @@
 function toggleTheme() {
-    console.log("toggle theme clicked");
     var currentTheme = document.documentElement.getAttribute("data-theme");
     if (currentTheme === "light") {
       targetTheme = "dark";
-      targetclassName = "fa-regular fa-moon";
-      console.log("dark");
+      targetclassName = "fa-regular fa-sun";
     } else {
       targetTheme = "light";
-      targetclassName = "fa-regular fa-sun";
-      console.log("light");
+      targetclassName = "fa-regular fa-moon";
     }
 
+    var toggle = document.getElementById("img-theme-toggle");
     toggle.className = targetclassName;
 
     document.documentElement.setAttribute('data-theme', targetTheme)
     localStorage.setItem('theme', targetTheme);
 };
+var storedTheme = localStorage.getItem('theme') || (window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light") || "light";
+document.documentElement.setAttribute('data-theme', storedTheme)
+
+document.addEventListener("DOMContentLoaded", function () {
+    var toggle = document.getElementById("img-theme-toggle");
+    var storedTheme = localStorage.getItem('theme') || (window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light") || "light";
+    document.documentElement.setAttribute('data-theme', storedTheme)
+    
+    if (toggle) {
+        if (storedTheme) {
+            if (storedTheme === "dark") {
+              toggle.className = "fa-regular fa-sun";
+            }
+            if (storedTheme === "light") {
+              toggle.className = "fa-regular fa-moon";
+            }
+        }
+    }
+});
